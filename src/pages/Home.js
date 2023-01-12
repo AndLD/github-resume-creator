247dc7a03d0e4bd2b879c66bed79da0ad1ba4186
AndLD <nil10035@gmail.com>
2023-01-12 22:30:48 +0200

    Home Page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,23 @@
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
 import '../styles/Home.scss'
 
 export default function Home() {
-    return <div className="home">Home</div>
+    const [username, setUsername] = useState('')
+
+    return (
+        <div className="home">
+            <form className="form">
+                <div className="label">Введіть username користувача GitHub, щоб отримати його резюме:</div>
+                <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+                <div>
+                    <Link to={username.length ? `/${username}` : '#'}>
+                        <button type="submit" onClick={() => {}}>
+                            Відкрити
+                        </button>
+                    </Link>
+                </div>
+            </form>
+        </div>
+    )
 }
