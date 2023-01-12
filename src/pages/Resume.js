b79a14a529b24e9f3abba784698930a77e8f3c56
AndLD <nil10035@gmail.com>
2023-01-12 23:30:34 +0200

    Resume Page Header

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,3 +1,49 @@
+import { LoadingOutlined } from '@ant-design/icons'
+import { Octokit } from 'octokit'
+import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import '../styles/Resume.scss'
+
 export default function Resume() {
-    return <div className="resume">Resume</div>
+    const params = useParams()
+
+    const [username] = useState(params.username)
+    const [user, setUser] = useState()
+
+    useEffect(() => {
+        console.log(username)
+        const octokit = new Octokit()
+
+        octokit.request(`GET /users/${username}`).then((value) => {
+            setUser(value.data)
+            console.log(value.data)
+        })
+    }, [])
+
+    if (!user) {
+        return (
+            <div className="loading">
+                <LoadingOutlined />
+            </div>
+        )
+    }
+
+    return (
+        <div className="resume">
+            <div className="layout">
+                <header>
+                    <div className="avatar">
+                        <img src={user.avatar_url} alt="GitHub user avatar" />
+                    </div>
+                    <div className="username">
+                        <div>{user.login}</div>
+                        <div>{user.name}</div>
+                    </div>
+                </header>
+
+                <main></main>
+                <footer></footer>
+            </div>
+        </div>
+    )
 }
